2afca1d1e7101925b2f7d48c551172aafca721a5
youjacal-hash <youjacal@gmail.com>
2026-07-10 21:32:36 -0300

    Update app.py

--- app.py
+++ app.py
@@ -404,10 +404,8 @@ def admin_update_points():
 
 # ─── Bootstrap & run ─────────────────────────────────────────────────────────
 
-with app.app_context():
-    db.create_all()
-    # seed_games()
-
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
